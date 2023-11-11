91a717b4e528931a5e276f341ca64ef5faf2e77e
Gamal Zayed <148896452+GMEY99@users.noreply.github.com>
2023-11-11 02:22:18 +0200

    Update pray timer.py

--- pray timer.py
+++ pray timer.py
@@ -7,14 +7,10 @@ def praying_time():
      month=int(input('Enter month: '))
      url = f'http://api.aladhan.com/v1/calendarByCity/{year}/{month}?city={city}&country={country}&method=2'
      response = requests.get(url)
-     # dd=response.content
      datax = response.json()
-     # Move the print statement before the return if you want to see the data
      x = datax["data"][0]
      y=x["timings"].items()
      z=[]
      for key, value in y:
           print(f'{key} : {value.strip('(EEST)')}')
-     # print(dd)
-praying_time()
 
